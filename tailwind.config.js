1bf6330c0be52b7fe96b86307795d3bbbb7f070f
Yohaan <yohanakipruto@gmail.com>
2025-03-23 16:15:38 +0300

    Initial Commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ['./src/**/*.{js,jsx,ts,tsx}', './index.html'],
   theme: {
-    extend: {},
+    extend: {
+      // Add customizations here
+    },
   },
   plugins: [],
-}
+};
 
